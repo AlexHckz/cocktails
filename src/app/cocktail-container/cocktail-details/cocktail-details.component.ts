9dfa638c6ccf313377516333663fb5fee3823bc8
AlexHckz <alexandre.cuneo@gmail.com>
2022-08-02 10:27:47 +0200

    refactorisationContainerCocktail

--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { Cocktail } from './interfaces/cocktail.interface';
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../../interfaces/cocktail.interface';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.sass'],
 })
-export class AppComponent {
+export class CocktailDetailsComponent implements OnInit {
   cocktail: Cocktail = {
     name: 'mojito',
     img: 'https://static.750g.com/images/640-420/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg',
     description:
       'Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français, est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche',
   };
+  constructor() {}
+
+  ngOnInit(): void {}
 }
