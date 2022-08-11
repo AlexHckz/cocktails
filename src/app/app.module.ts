dc73f6297eab8d5b14a1924fced298d877ac90f2
AlexHckz <alexandre.cuneo@gmail.com>
2022-08-11 22:24:30 +0200

    creating cocktail service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { ColorDirective } from './color.directive';
+import { SelectedDirective } from './shared/directives/selected.directive';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { CocktailContainerComponent } from './cocktail-container/cocktail-contai
     CocktailDetailsComponent,
     CocktailListComponent,
     CocktailContainerComponent,
+    ColorDirective,
+    SelectedDirective,
   ],
   imports: [BrowserModule],
   providers: [],
